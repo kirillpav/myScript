68cdccfd34fb4e518513a808d3674fa095ed6e9b
Kirill Pavlov <90044196+kirillpav@users.noreply.github.com>
2026-02-14 14:09:08 -0800

    Initial Commit

---
+++ myScript/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  myScript
+//
+//  Created by Kirill Pavlov on 2/14/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ myScriptTests/myScriptTests.swift
@@ -0,0 +1,17 @@
+//
+//  myScriptTests.swift
+//  myScriptTests
+//
+//  Created by Kirill Pavlov on 2/14/26.
+//
+
+import Testing
+@testable import myScript
+
+struct myScriptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
